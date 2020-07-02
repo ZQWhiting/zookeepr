4ef250618c91b6a0f30834235c738c74259bd22b
ZQWhiting <zach.whiting@icloud.com>
2020-07-01 20:40:53 -0600

    added params by id

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { animals } = require('./data/animals.json');
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -33,7 +33,13 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults
-}
+};
+
+function findById (id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
+
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -42,6 +48,15 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
