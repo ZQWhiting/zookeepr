e642cac49a7770f5a2b8cec30b157c77ffe7f063
ZQWhiting <zach.whiting@icloud.com>
2020-07-03 10:51:34 -0600

    added functionality for adding zookeepers through frontend

--- routes/apiRoutes/zookeeperRoutes.js
+++ routes/apiRoutes/zookeeperRoutes.js
@@ -16,10 +16,20 @@ router.get('/zookeepers', (req, res) => {
 })
 
 router.get('/zookeepers/:id', (req, res) => {
+    const result = findById(req.params.id, zookeepers);
+    if (result) {
+        res.json(result)
+    }
+    else {
+        res.send(404);
+    }
+})
+
+router.post('/zookeepers', (req, res) => {
     req.body.id = zookeepers.length.toString();
 
     if (!validateZookeeper(req.body)) {
-        res.status(400).send("The zookeeper is not properly formatted.");
+        res.status(400).send("The zookeeper is not properly formatted.")
     } else {
         const zookeeper = createNewZookeeper(req.body, zookeepers);
         res.json(zookeeper);
